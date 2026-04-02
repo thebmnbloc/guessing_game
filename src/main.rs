824ae48b6f739b56e60319d84168e58f5c0a8ecc
thebmnbloc <bioralak46@gmail.com>
2026-04-02 13:48:29 +0200

    add comparison logic

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ extern crate rand;
 
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the number!");
@@ -17,6 +18,15 @@ fn main() {
     io::stdin().read_line(&mut guess)
     .expect("Failed to read line");
 
+    let guess: u32 = guess.trim().parse()
+        .expect("Please enter a number!");
+
     println!("You guessed: {}", guess);
+
+     match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
 
